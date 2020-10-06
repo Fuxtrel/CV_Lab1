163e750060516b9ac586a06617d836569e65d6a5
Fuxtrel <a99som@yandex.ru>
2020-10-06 16:44:33 +0300

    Add robot tower with turn, but without motion

--- my_robot/my_robot.cpp
+++ my_robot/my_robot.cpp
@@ -1,9 +1,9 @@
 #include "my_robot.h"
 
 
-Robot::Robot(const Size2f m_areaSize, const Size2f &m_robotBodySize, const Size2f &m_robotWheelSize, float motionSpeed)
-        : m_robotMotionSpeed(motionSpeed), m_robotRotateAngle(30 * M_PI / 180), m_directionOfRotation(1),
-          m_currentAngle(0), deltaY(1), deltaX(1)
+Robot::Robot(const Size2f m_areaSize, const Size2f robotBodySize, const Size2f &m_robotWheelSize, float motionSpeed, float rotationSpeed)
+        : m_robotMotionSpeed(motionSpeed), m_robotRotateAngle(rotationSpeed * M_PI / 180), m_directionOfRotation(1),
+          m_currentAngle(0), m_areaSize(m_areaSize), m_robotRotationSpeed(rotationSpeed), m_robotBodySize(robotBodySize)
 {
 
     m_robotBody[0].x = (m_areaSize.width / 2) - (m_robotBodySize.width / 2);
@@ -45,6 +45,33 @@ Robot::Robot(const Size2f m_areaSize, const Size2f &m_robotBodySize, const Size2
     m_robotBody[15].y = m_robotBody[2].y + m_robotWheelSize.height;
     m_robotBody[16].x = m_robotBody[0].x + (m_robotBodySize.width / 2);
     m_robotBody[16].y = m_robotBody[0].y - (m_robotBodySize.height / 2);
+    /*******************************************************************/
+    m_robotTower[0].x = m_robotBody[16].x - (m_robotBodySize.width / 4);
+    m_robotTower[1].x = m_robotBody[16].x + (m_robotBodySize.width / 4);
+    m_robotTower[2].x = m_robotBody[16].x + (m_robotBodySize.width / 3);
+    m_robotTower[3].x = m_robotBody[16].x + (m_robotBodySize.width / 4);
+    m_robotTower[4].x = m_robotBody[16].x - (m_robotBodySize.width / 4);
+    m_robotTower[5].x = m_robotBody[16].x - (m_robotBodySize.width / 3);
+
+    m_robotTower[0].y = m_robotBody[16].y - (m_robotBodySize.height / 4);
+    m_robotTower[1].y = m_robotBody[16].y - (m_robotBodySize.height / 4);
+    m_robotTower[3].y = m_robotBody[16].y + (m_robotBodySize.height / 4);
+    m_robotTower[4].y = m_robotBody[16].y + (m_robotBodySize.height / 4);
+    m_robotTower[2].y = m_robotBody[16].y;
+    m_robotTower[5].y = m_robotBody[16].y;
+
+    m_robotTower[6].x = m_robotBody[16].x - (m_robotBodySize.width / 16);
+    m_robotTower[7].x = m_robotBody[16].x - (m_robotBodySize.width / 16);
+    m_robotTower[8].x = m_robotBody[16].x + (m_robotBodySize.width / 16);
+    m_robotTower[9].x = m_robotBody[16].x + (m_robotBodySize.width / 16);
+    m_robotTower[10].x = m_robotBody[16].x;
+
+    m_robotTower[6].y = m_robotTower[0].y;
+    m_robotTower[7].y = m_robotTower[0].y - (m_robotBodySize.width / 6);
+    m_robotTower[8].y = m_robotTower[0].y - (m_robotBodySize.width / 6);
+    m_robotTower[9].y = m_robotTower[0].y;
+    m_robotTower[10].y = m_robotBody[16].y;
+
 
     robotColor = {0, 0, 0};
     color = {0, 100, 0};
@@ -62,29 +89,41 @@ Robot::Robot(const Size2f m_areaSize, const Size2f &m_robotBodySize, const Size2
 
 void Robot::drownRobot(Scalar color)
 {
-    line(img, m_robotBody[0], m_robotBody[1], color, 3);
-    line(img, m_robotBody[1], m_robotBody[2], color, 3);
-    line(img, m_robotBody[2], m_robotBody[3], color, 3);
-    line(img, m_robotBody[3], m_robotBody[0], color, 3);
+    line(img, m_robotBody[0], m_robotBody[1], color, 2);
+    line(img, m_robotBody[1], m_robotBody[2], color, 2);
+    line(img, m_robotBody[2], m_robotBody[3], color, 2);
+    line(img, m_robotBody[3], m_robotBody[0], color, 2);
 
-    line(img, m_robotBody[2], m_robotBody[11], color, 3);
-    line(img, m_robotBody[3], m_robotBody[14], color, 3);
+    line(img, m_robotBody[2], m_robotBody[11], color, 2);
+    line(img, m_robotBody[3], m_robotBody[14], color, 2);
 
-    line(img, m_robotBody[0], m_robotBody[4], color, 3);
-    line(img, m_robotBody[4], m_robotBody[5], color, 3);
-    line(img, m_robotBody[5], m_robotBody[6], color, 3);
+    line(img, m_robotBody[0], m_robotBody[4], color, 2);
+    line(img, m_robotBody[4], m_robotBody[5], color, 2);
+    line(img, m_robotBody[5], m_robotBody[6], color, 2);
 
-    line(img, m_robotBody[1], m_robotBody[7], color, 3);
-    line(img, m_robotBody[7], m_robotBody[8], color, 3);
-    line(img, m_robotBody[8], m_robotBody[9], color, 3);
+    line(img, m_robotBody[1], m_robotBody[7], color, 2);
+    line(img, m_robotBody[7], m_robotBody[8], color, 2);
+    line(img, m_robotBody[8], m_robotBody[9], color, 2);
 
-    line(img, m_robotBody[2], m_robotBody[10], color, 3);
-    line(img, m_robotBody[10], m_robotBody[11], color, 3);
-    line(img, m_robotBody[11], m_robotBody[12], color, 3);
+    line(img, m_robotBody[2], m_robotBody[10], color, 2);
+    line(img, m_robotBody[10], m_robotBody[11], color, 2);
+    line(img, m_robotBody[11], m_robotBody[12], color, 2);
 
-    line(img, m_robotBody[3], m_robotBody[13], color, 3);
-    line(img, m_robotBody[13], m_robotBody[14], color, 3);
-    line(img, m_robotBody[14], m_robotBody[15], color, 3);
+    line(img, m_robotBody[3], m_robotBody[13], color, 2);
+    line(img, m_robotBody[13], m_robotBody[14], color, 2);
+    line(img, m_robotBody[14], m_robotBody[15], color, 2);
+
+    line(img, m_robotTower[0], m_robotTower[1], color, 2);
+    line(img, m_robotTower[1], m_robotTower[2], color, 2);
+    line(img, m_robotTower[2], m_robotTower[3], color, 2);
+    line(img, m_robotTower[3], m_robotTower[4], color, 2);
+    line(img, m_robotTower[4], m_robotTower[5], color, 2);
+    line(img, m_robotTower[5], m_robotTower[0], color, 2);
+    line(img, m_robotTower[6], m_robotTower[7], color, 2);
+    line(img, m_robotTower[7], m_robotTower[8], color, 2);
+    line(img, m_robotTower[8], m_robotTower[9], color, 2);
+    line(img, m_robotTower[9], m_robotTower[6], color, 2);
+
     if (color == robotColor)
     {
         imshow("Empty window", img);
@@ -191,17 +230,31 @@ void Robot::robotMotion()
                 break;
             case 'e':
                 m_directionOfRotation = 1;
-                m_currentAngle -= 30;
+                m_currentAngle -= m_robotRotationSpeed;
                 drownRobot(color);
                 saveArray();
-                robotRotation();
+                robotRotation(m_robotBody, 17);
                 break;
             case 'q':
                 m_directionOfRotation = -1;
-                m_currentAngle += 30;
+                m_currentAngle += m_robotRotationSpeed;
                 drownRobot(color);
                 saveArray();
-                robotRotation();
+                robotRotation(m_robotBody, 17);
+                break;
+            case 'j':
+                m_directionOfRotation = -1;
+                m_currentAngle += m_robotRotationSpeed;
+                drownRobot(color);
+                saveArray();
+                robotRotation(m_robotTower, 11);
+                break;
+            case 'l':
+                m_directionOfRotation = 1;
+                m_currentAngle += m_robotRotationSpeed;
+                drownRobot(color);
+                saveArray();
+                robotRotation(m_robotTower, 11);
                 break;
             case 27:
                 exit(0);
@@ -211,21 +264,21 @@ void Robot::robotMotion()
     }
 }
 
-void Robot::robotRotation()
+void Robot::robotRotation(Point2f *array, int length)
 {
     float tmp_x = 0, tmp_y = 0;
-    for (int i = 0; i < 16; i++)
+    for (int i = 0; i < length - 1; i++)
     {
-        m_robotBody[i].x -= m_robotBody[16].x;
-        m_robotBody[i].y -= m_robotBody[16].y;
-        tmp_x = m_robotBody[i].x;
-        tmp_y = m_robotBody[i].y;
-        m_robotBody[i].x = (tmp_x * cos(m_robotRotateAngle) +
+        array[i].x -= array[length - 1].x;
+        array[i].y -= array[length - 1].y;
+        tmp_x = array[i].x;
+        tmp_y = array[i].y;
+        array[i].x = (tmp_x * cos(m_robotRotateAngle) +
                             tmp_y * m_directionOfRotation * (-sin(m_robotRotateAngle)));
-        m_robotBody[i].y = (tmp_x * m_directionOfRotation * sin(m_robotRotateAngle) +
+        array[i].y = (tmp_x * m_directionOfRotation * sin(m_robotRotateAngle) +
                             tmp_y * (cos(m_robotRotateAngle)));
-        m_robotBody[i].x += m_robotBody[16].x;
-        m_robotBody[i].y += m_robotBody[16].y;
+        array[i].x += array[length - 1].x;
+        array[i].y += array[length - 1].y;
     }
     borderCheck();
     drownRobot(robotColor);
@@ -236,8 +289,8 @@ void Robot::borderCheck()
 {
     for (int i = 4; i < 14; i += 3)
     {
-        if ((m_robotBody[i].x >= 1920) || (m_robotBody[i].x <= 0) ||
-            (m_robotBody[i].y >= 1080) || m_robotBody[i].y <= 0)
+        if ((m_robotBody[i].x >= m_areaSize.width) || (m_robotBody[i].x <= 0) ||
+            (m_robotBody[i].y >= m_areaSize.height) || m_robotBody[i].y <= 0)
         {
             for (int j = 0; j < 17; j++)
             {
--- my_robot/my_robot.h
+++ my_robot/my_robot.h
@@ -5,7 +5,6 @@
 #include "opencv2/core.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
-
 #include <cmath>
 
 using namespace std;
@@ -15,37 +14,34 @@ class Robot{
 
 public:
     Robot() = default;
-    Robot(const Size2f m_areaSize, const Size2f &m_robotBodySize, const Size2f &m_robotWheelSize, float motionSpeed);
+    Robot(const Size2f m_areaSize, const Size2f robotBodySize, const Size2f &m_robotWheelSize, float motionSpeed, float m_robotRotationSpeed);
     void playRobot();
     void drownRobot(Scalar color);
 
 private:
     const Size m_areaSize;
     const Size m_robotBodySize;
-    const Size m_robotWheelSize;
+    float m_robotRotationSpeed;
     float m_robotMotionSpeed;
     float m_robotRotateAngle;
     int m_currentAngle;
+    float m_rotationMatrix[6];
+    int m_directionOfRotation;
     Point2f m_tmpRobotBody[17];
     Point2f m_robotBody[17];
+    Point2f m_robotTower[11];
     Mat img;
     Scalar robotColor;
     Scalar color;
-    float m_rotationMatrix[6];
-    int m_directionOfRotation;
-    float deltaY;
-    float deltaX;
 
-    void robotRotation();
+
+    void robotRotation(Point2f *array, int length);
 
     void robotMotion();
 
     void borderCheck();
 
     void saveArray();
-
-
-
 
 
 };
